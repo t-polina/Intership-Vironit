300a72643ec5a4218ec8d93bcb226ac5d213c0cf
ZubelRuslan <rusalexd@gmail.com>
2019-09-12 09:11:56 +0300

    Update BinaryTreeUI.js

--- 4. Data Structures UI/BinaryTreeUI.js
+++ 4. Data Structures UI/BinaryTreeUI.js
@@ -6,28 +6,28 @@ class BinatyTreeUI {
          this.parent.append(this.treeView);
     }
     createNodeTree(root, value, leftOrRight) {
-        if (root == value) {
+        if (root == value) { // Please use strict eq
             return 0;
         }
         else if (root === true) {
             this.rootView = document.createElement('div');
             this.rootView.className = `root ${String(value)}`;
-            this.rootView.id = `${String(value)}`;
+            this.rootView.id = `${String(value)}`; //String constructor unnecessary with string templates
             this.rootView.innerText = value;
             this.treeView.append(this.rootView);
         }
         else {
             this.rootView = document.createElement('div');           
-            this.rootView.id = `${String(value)}`;
+            this.rootView.id = `${String(value)}`; //String constructor unnecessary with string templates
             this.rootView.innerText = value;
          
             if(!leftOrRight){
-                this.rootView.className = `left`;
-                document.getElementById(`${String(root)}`).append(this.rootView);
+                this.rootView.className = `left`; // Please use simple single quotes for simple strings
+                document.getElementById(`${String(root)}`).append(this.rootView); //String constructor unnecessary with string templates
             }
             else{
-                this.rootView.className = `right`;
-                document.getElementById(`${String(root)}`).append(this.rootView);
+                this.rootView.className = `right`; // Please use simple single quotes for simple strings
+                document.getElementById(`${String(root)}`).append(this.rootView); //String constructor unnecessary with string templates
             }  
         }
 
